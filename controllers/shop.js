94ceec6d3a9cd4e4e56bbb6bd69395651d5d1cf9
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2022-10-29 11:25:41 -0500

    feat(payments) implement no functional payments with stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -124,6 +125,92 @@ exports.postCartDeleteProduct = (req, res) => {
       res.redirect('/cart');
     })
     .catch((err) => console.log(err));
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let totalSum = 0;
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      products = user.cart.items;
+      totalSum = 0;
+      products.forEach(({ quantity, productId }) => {
+        totalSum += quantity * productId.price;
+      });
+
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products,
+        totalSum,
+        // sessionId: session.id,
+      });
+
+      // return stripe.checkout.sessions.create({
+      //   payment_method_types: ['card'],
+      //   line_items: products.map(({ quantity, productId }) => {
+      //     const { title, description, price } = productId;
+      //     //     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+      //     //     price: '{{PRICE_ID}}',
+      //     //     quantity: 1,
+      //     // This change in new version of stripe, and this will not fix in this project. Search the stripe doc
+      //     // return {
+      //     //   name: title,
+      //     //   description,
+      //     //   amount: price * 100,
+      //     //   currency: 'USD',
+      //     //   quantity,
+      //     // };
+      //   }),
+      //   success_url: `${req.protocol}://${req.get('host')}/checkout/success`,
+      //   cancel_url: `${req.protocol}://${req.get('host')}/checkout/cancel`,
+      // });
+    })
+    // .then((session) => {
+    //   res.render('shop/checkout', {
+    //     path: '/checkout',
+    //     pageTitle: 'Checkout',
+    //     products,
+    //     totalSum,
+    //     sessionId: session.id,
+    //   });
+    // })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res) => {
+  const loggedUser = req.user;
+  loggedUser
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: loggedUser.name,
+          userId: loggedUser,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      return loggedUser.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postOrder = (req, res) => {
